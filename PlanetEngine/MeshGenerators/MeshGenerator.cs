fa3e818126fb376ed1cb214600cee3bcb0a0ea08
Argyraspides <joshivinayak441@gmail.com>
2025-06-08 22:06:35 +1200

    Nightly save

--- PlanetEngine/MeshGenerators/MeshGenerator.cs
+++ PlanetEngine/MeshGenerators/MeshGenerator.cs
@@ -4,8 +4,26 @@ namespace Gaia.PlanetEngine.MeshGenerators;
 
 public static partial class MeshGenerator
 {
-    public static ArrayMesh GenerateWebMercatorMesh()
+    
+    // Generates a plane mesh. Each vertex corresponds to a pixel
+    // so that you can use height maps to deform the mesh to create
+    // terrain
+    // width and height are for how big the map tile is going to be.
+    // Common size is 256x256 as that is the size given back by most
+    // map provider apis
+    public static ArrayMesh GenerateWebMercatorMesh(int width, int height)
     {
-        return new ArrayMesh();
+        var arrayMesh = new ArrayMesh();
+        var arrays = new Godot.Collections.Array();
+        arrays.Resize((int)Mesh.ArrayType.Max);
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                
+            }
+        }
+        
     }
 }
